f9083377005386a0806a330da5d3dbb0665252cb
Fernando Rodrigues <fernando.rodrigues@branden.pt>
2020-04-26 18:09:17 +0100

    regenerated with dev postgresql

--- src/main/java/com/mycompany/myapp/config/SecurityConfiguration.java
+++ src/main/java/com/mycompany/myapp/config/SecurityConfiguration.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Import;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -40,11 +39,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     public SecurityConfiguration(JHipsterProperties jHipsterProperties, SecurityProblemSupport problemSupport) {
         this.problemSupport = problemSupport;
         this.jHipsterProperties = jHipsterProperties;
-    }
-    @Override
-    public void configure(WebSecurity web) {
-        web.ignoring()
-            .antMatchers("/h2-console/**");
     }
 
     @Override
